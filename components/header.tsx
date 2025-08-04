d4a0e0d2f547b20a60edd9e146339a5eb8362651
codercarl1243 <codercarl1243@gmail.com>
2025-08-04 11:18:54 +1000

    temp changes

--- components/header.tsx
+++ components/header.tsx
@@ -1,20 +1,27 @@
 import { RiFacebookLine, RiInstagramLine } from "@remixicon/react";
 import Icon from "./icon";
-import Logo from "./logo";
 import SkipLink from "./skiplink";
 import clsx from "clsx";
 
 
-export default function Header({className, ...props}: React.ComponentProps<'header'>) {
+export default function Header({ className, ...props }: React.ComponentProps<'header'>) {
 
     return (
         <header className={clsx(className, "flex justify-between p-4")} {...props}>
             <SkipLink />
-            {/* <Logo /> */}
-            {/* this might end up being a nav but is a div for now */}
-            <div className="sm:ml-auto nav-items flex gap-2 w-full sm:w-fit">
-                <a className="button link-button" href="https://www.facebook.com/events/1504819507558539/" >{<Icon icon={RiFacebookLine} size={'xl'} color={"blue"} />}</a>
-                <a className="button link-button ml-auto sm:ml-0" href="instagram.com">{<Icon icon={RiInstagramLine} size={'xl'} color={"blue"} />}</a>
+            <div className="sm:ml-auto nav-items flex flex-wrap items-center justify-between gap-2 w-full sm:w-fit">
+                <div className="flex">
+                    <a className="button link-button" href="https://www.facebook.com/events/1504819507558539/" >
+                        <span className="sr-only">Facebook</span>
+                        <Icon icon={RiFacebookLine} size={'xl'} color={"blue"} />
+                    </a>
+                    {/* <a className="button link-button sm:ml-0" href="instagram.com">
+                        <span className="sr-only">Instagram</span>
+                        <Icon icon={RiInstagramLine} size={'xl'} color={"blue"} />
+                    </a> */}
+                </div>
+                <a className="h-fit" href="mailto:admin@vitaliahealth.com.au">admin@vitaliahealth.com.au</a>
+
             </div>
         </header>
     )
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,23 +1,24 @@
 import clsx from "clsx"
+import Image from "next/image"
 import React from "react"
 
 
-export default function Hero({ className, ...props }: React.ComponentProps<'section'>) {
+export default function Hero({ className, ...props }: React.ComponentProps<'div'>) {
 
     return (
-        <section className={clsx(className, 
+        <div className={clsx(className, 
             "grid grid-cols-1 grid-rows-[auto] gap-2",
             "md:grid-cols-[2rem_2fr_2fr_3fr_2rem] md:grid-rows-2 items-center"
         )} {...props}>
-            <img src="/HeroImage.png" alt="" aria-label="Bumps, Babies & Beyond" className={
+            <Image src="/HeroImage.png" alt="Bumps, Babies & Beyond" width={1000} height={750} aria-label="Bumps, Babies & Beyond" className={
                 clsx("w-full h-auto object-cover row-1", 
                     "md:row-[1/3] md:col-start-2 md:col-span-2"
             )}/>
-            <img src="/event_details.png" alt="" className={
+            <Image src="/event_details.png" width={500} height={250} alt="" className={
                 clsx("w-full h-auto object-cover",
                 "md:row-1 md:col-4")} />
             <EventSweeteners className="md:row-2 md:col-4"/>
-        </ section>
+        </ div>
 
              // <LogoAsText className="text-3xl font-accent leading-none font-medium w-full text-center col-span-2 row-span-full" /> 
              // <div className="flex flex-col items-center gap-8 md:items-start"> 
@@ -28,67 +29,72 @@ export default function Hero({ className, ...props }: React.ComponentProps<'sect
     )
 }
 
-function LogoAsText(props: React.ComponentProps<'div'>) {
-    return (
-        <div {...props}>
-            {/* <img src="/main_hero.png" alt="" className="w-full" /> */}
-            <img src="/HeroImage.png" alt="" aria-label="Bumps, Babies & Beyond"/>
-           <h1 className="sr-only"> <span className="text-yellow-400 block text-center">Bumps,</span>
-            <div>
-                <span className="text-purple-400">Bab</span>
-                <span className="text-secondary-400/70">ie</span>
-                <span className="text-yellow-400">s</span>
-            </div>
-            <div className="text-secondary-400">&</div>
-            <span className="text-purple-400">Beyond</span></h1>
-        </div>
-    )
-}
-function EventDetails({ className }: React.ComponentProps<'img'>) {
+// function LogoAsText(props: React.ComponentProps<'div'>) {
+//     return (
+//         <div {...props}>
+//             {/* <img src="/main_hero.png" alt="" className="w-full" /> */}
+//             <img src="/HeroImage.png" alt="" aria-label="Bumps, Babies & Beyond"/>
+//            <h1 className="sr-only"> <span className="text-yellow-400 block text-center">Bumps,</span>
+//             <div>
+//                 <span className="text-purple-400">Bab</span>
+//                 <span className="text-secondary-400/70">ie</span>
+//                 <span className="text-yellow-400">s</span>
+//             </div>
+//             <div className="text-secondary-400">&</div>
+//             <span className="text-purple-400">Beyond</span></h1>
+//         </div>
+//     )
+// }
+// function EventDetails({ className }: React.ComponentProps<'img'>) {
 
-    return (
-            <img src="/event_details.png" alt="" className={clsx(className, "min-w-[250px]")} />
-        // <div id="details_one" className={clsx(className, "w-full p-4 text-center text-xl leading-[1.15] text-light bg-secondary-700/80")}>
-        //     <dl className="sr-only">
-        //         <div className="pt-4">
-        //             <dt className="sr-only">Date</dt>
-        //             <dd>Saturday 1st November</dd>
-        //         </div>
-        //         <div>
-        //             <dt className="sr-only">Location</dt>
-        //             <dd>Palmwoods Memorial Hall</dd>
-        //         </div>
-        //         <div>
-        //             <dt className="sr-only">Time</dt>
-        //             <dd>10am–3pm</dd>
-        //         </div>
-        //         <div className="pb-4">
-        //             <dt className="sr-only">Entry</dt>
-        //             <dd>Free Entry!</dd>
-        //         </div>
-        //     </dl>
-        // </div>
-    )
-}
+//     return (
+//             <img src="/event_details.png" alt="" className={clsx(className, "min-w-[250px]")} />
+//         // <div id="details_one" className={clsx(className, "w-full p-4 text-center text-xl leading-[1.15] text-light bg-secondary-700/80")}>
+//         //     <dl className="sr-only">
+//         //         <div className="pt-4">
+//         //             <dt className="sr-only">Date</dt>
+//         //             <dd>Saturday 1st November</dd>
+//         //         </div>
+//         //         <div>
+//         //             <dt className="sr-only">Location</dt>
+//         //             <dd>Palmwoods Memorial Hall</dd>
+//         //         </div>
+//         //         <div>
+//         //             <dt className="sr-only">Time</dt>
+//         //             <dd>10am–3pm</dd>
+//         //         </div>
+//         //         <div className="pb-4">
+//         //             <dt className="sr-only">Entry</dt>
+//         //             <dd>Free Entry!</dd>
+//         //         </div>
+//         //     </dl>
+//         // </div>
+//     )
+// }
 
 function EventSweeteners({ className }: React.ComponentProps<'ul'>) {
+  const sweeteners = [
+    { icon: '🎓', label: 'Local Experts' },
+    { icon: '🎁', label: 'A free raffle for families' },
+    { icon: '🎨', label: 'Free Face Painting' },
+    { icon: '🫧', label: 'Bubble Station' },
+    { icon: '🐣', label: 'A free soft play zone for little ones' },
+    { icon: '🎲', label: 'Games & Gifts' },
+  ]
 
-    return (
-            <ul className={clsx(className, "w-full grid grid-cols-2", 
-                "sm:grid-cols-3 gap-4 max-w-4xl")}>
-                {[
-                    ['🎓', <span>Local Experts</span>],
-                    ['🎁', <span>A free raffle for families</span>],
-                    ['🎨', <span>free Face Painting</span>],
-                    ['🫧', <span>Bubble Station</span>],
-                    ['🐣', <span>A free soft play zone for little ones</span>],
-                    ['🎲', <span>Games & Gifts</span>],
-                ].map(([icon, label], idx) => (
-                    <li key={idx} className="flex items-center gap-2 min-w-[12ch]">
-                        <span>{icon}</span>
-                        {label}
-                    </li>
-                ))}
-            </ul>
-    )
+  return (
+    <ul
+      className={clsx(
+        className,
+        'w-full grid grid-cols-2 sm:grid-cols-3 gap-4 max-w-4xl'
+      )}
+    >
+      {sweeteners.map((sweetener, idx) => (
+        <li key={idx} className="flex items-center gap-2 min-w-[12ch]">
+          <span>{sweetener.icon}</span>
+          <span>{sweetener.label}</span>
+        </li>
+      ))}
+    </ul>
+  )
 }
