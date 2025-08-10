fb0f39574fec028495f74a037c1b08a296a6812a
codercarl1243 <codercarl1243@gmail.com>
2025-08-11 08:36:27 +1000

    CLEANUP assets in public

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,6 +3,6 @@ import Image from '@/components/image';
 export default function Logo(){
 
     return (
-        <Image src="/Logo.webp" alt="" width={150} height={100}/>
+        <Image src="/assets/logos/Logo.webp" alt="" width={150} height={100}/>
     )
 }
